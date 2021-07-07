f9e13ab57bcc4dd2c2e0eee754b8d0e2ca3f4425
hhhoh <hhhoh@naver.com>
2021-07-07 18:18:44 +0900

    Spring Boot BackEnd Server, static file will be deleted.

---
+++ src/main/java/com/rest/api/controller/GreetingController.java
@@ -0,0 +1,19 @@
+package com.rest.api.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+
+
+	@MessageMapping("/hello")
+	@SendTo("/topic/greetings")
+	public com.rest.api.controller.Greeting greeting(HelloMessage message) throws Exception {
+		Thread.sleep(1000); // simulated delay
+		return new com.rest.api.controller.Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+	}
+
+}
