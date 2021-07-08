93388de13429e208a504be96360830cd5b3fad1b
YOUNGSEO-PARK <irene0seo97@gmail.com>
2021-07-09 02:16:30 +0900

    url mapping & user login fin.

---
+++ src/main/java/com/rest/api/controller/ViewController.java
@@ -0,0 +1,20 @@
+package com.rest.api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class ViewController {
+
+    @GetMapping("/index") //세부적인 url mapping
+    public String indexpage(Model model, HttpSession session) {
+        return "index";
+    }
+    @GetMapping("/login")
+    public String loginpage(Model model, HttpSession session) {
+        return "login";
+    }
+}
